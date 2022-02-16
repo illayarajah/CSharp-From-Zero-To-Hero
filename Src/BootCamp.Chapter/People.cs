6947c8a230114188ef567536804c94299f3bdbd4
Raja Y <illayarajah@hotmail.com>
2022-02-15 22:19:33 -0500

    added people

---
+++ Src/BootCamp.Chapter/People.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace BootCamp.Chapter
+{
+    public class People
+    {
+
+        string name;
+        int[] accountHistory;
+
+        public People(string person)
+        {
+            string[] tmp = person.Split(',');
+            for (int i = 0; i < tmp.Length; i++)
+            {
+                if (i == 0)
+                {
+                    name = tmp[i];
+                }
+                else
+                    Int32.TryParse(Regex.Replace(tmp[i], "[^0-9/.]+", ""), out accountHistory[i - 1]);
+
+            }
+
+
+        }
+
+
+
+
+
+    }
+}
